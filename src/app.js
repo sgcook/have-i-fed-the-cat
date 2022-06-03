60b773b3aef48fafab98b138db8ef7144c8832b5
Sara Cook <saracook087@gmail.com>
2022-06-03 12:51:51 +0100

    added .catch() blocks

--- src/app.js
+++ src/app.js
@@ -6,28 +6,41 @@ const app = express();
 app.use(express.json());
 
 app.post('/cats', (req, res) => {
-  Cat.create(req.body).then(cat => res.status(201).json(cat));
+  Cat.create(req.body)
+    .then(cat => res.status(201).json(cat))
+    .catch(res.status(400));
 });
 
 app.get('/cats', (req, res) => {
-  Cat.findAll({where: req.query}).then(cats => res.status(200).json(cats));
+  Cat.findAll({where: req.query})
+    .then(cats => res.status(200).json(cats))
+    .catch(res.status(400));
 });
 
 app.get('/cats/:catId', (req, res) => {
-  Cat.findByPk(req.params.catId).then(cat => res.status(200).json(cat));
+  Cat.findByPk(req.params.catId)
+    .then(cat => res.status(200).json(cat))
+    .catch(res.status(400));
 });
 
 app.patch('/cats/:catId', (req, res) => {
-  Cat.update(req.body, {where: {id: req.params.catId} }).then(cat => res.status(200).json(cat));
+  Cat.update(req.body, {where: {id: req.params.catId} })
+    .then(cat => res.status(200).json(cat))
+    .catch(res.status(400));
 });
 
 app.delete('/cats/:catId', (req, res) => {
   const ID_TO_DELETE = req.params.catId;
-  Cat.destroy({ where: { id: ID_TO_DELETE } }).then(cat => res.status(200).json(cat));
+  Cat.destroy({ where: { id: ID_TO_DELETE } })
+    .then(cat => res.status(200).json(cat))
+    .catch(res.status(400));
+
 });
 
 app.patch('/feed/:catId', (req, res) => {
-  Cat.update({lastFed: new Date()}, {where: {id: req.params.catId} }).then(cat => res.status(200).json(cat));
+    Cat.update({lastFed: new Date()}, {where: {id: req.params.catId} })
+      .then(cat => res.status(200).json(cat))
+      .catch(res.status(400));
 })
 
 module.exports = app;
