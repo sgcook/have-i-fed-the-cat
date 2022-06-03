6d49115af928ef8ee787496fa58922830c149272
Sara Cook <saracook087@gmail.com>
2022-06-03 12:25:11 +0100

    cats can be saved in database

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const { Cat } = require('../src/models');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/cats', (req, res) => {
-  res.status(201).json(req.body);
+  Cat.create(req.body).then(cat => res.status(201).json(cat));
+})
+
+app.get('/cats', (req, res) => {
+  Cat.findAll(req.body).then(cats => res.status(200).json(cats));
+})
+
+app.get('/cats/:catId', (req, res) => {
+  Cat.findByPk(req.params.catId).then(cat => res.status(200).json(cat));
 })
 
 module.exports = app;
